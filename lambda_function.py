47dfb1e9ec8d27c1817dade4885fc190c45c311f
sufyaniqbal <sufyaniqbal@me.com>
2019-10-18 13:00:26 +0100

    fixed error in test

--- lambda_function.py
+++ lambda_function.py
@@ -3,9 +3,6 @@ import json
 import requests
 
 
-business_layer_endpoint = '/contributor/validationOutputSave'
-business_layer_endpoint_local = 'http://192.168.99.113:31303/contributor/validationOutputSave'
-
 def lambda_handler(event, context):
     print('Event: ' + str(event))
     queue_data = event['Records'][0]['body']
@@ -14,6 +11,9 @@ def lambda_handler(event, context):
 
     data_to_business_layer = parse_validation_save_data(data_to_be_parsed)
 
+    business_layer_endpoint = '/contributor/validationOutputSave'
+    business_layer_endpoint_local = 'http://192.168.99.113:31303/contributor/validationOutputSave'
+
     try:
         request_response = requests.post(business_layer_endpoint, data_to_business_layer)
         print('Response: ' + str(request_response))
@@ -21,9 +21,10 @@ def lambda_handler(event, context):
         print(request_response.content, "CONTENT")
         print(request_response.status_code, "STATUS CODE")
     except:
+        request_response = ''
         print("{\"Error\": \"Problem with call to Business Layer\"}")
         print('Response: ' + str(request_response))
         print(request_response.content, "CONTENT")
         print(request_response.text, "TEXT")
         print(request_response.status_code, "STATUS CODE")
-        return(request_response.content)
+        return request_response.content
--- tests/test_queue_output.py
+++ tests/test_queue_output.py
@@ -1,48 +1,50 @@
 import pytest
-import json
 from parse_validation import parse_validation_save_data
 
 
 @pytest.fixture
-def input_data():
-    [{'formula': '343.3 != ""',
-    'metadata': {'validation': 'Value present',
-    'reference': '70001',
-    'period': '201912',
-    'survey': '003',
-    'validationid': 23412,
-    'bpmid': ''},
-    'triggered': 'true'},
-    {'formula': '12.3 != ""',
-    'metadata': {'validation': 'Value present',
-    'reference': '70001',
-    'period': '201912',
-    'survey': '003',
-    'validationid': 23413,
-    'bpmid': ''},
-    'triggered': 'true'}]
+def json_input():
+    return[
+        {'formula': '343.3 != ""',
+        'metadata': {'validation': 'Value present',
+        'reference': '70001',
+        'period': '201912',
+        'survey': '003',
+        'validationid': 23412,
+        'bpmid': ''},
+        'triggered': True},
+        {'formula': '12.3 != ""',
+        'metadata': {'validation': 'Value present',
+        'reference': '70001',
+        'period': '201912',
+        'survey': '003',
+        'validationid': 23413,
+        'bpmid': ''},
+        'triggered': True}]
 
 
 @pytest.fixture
-def output_data():
-    {'validation_outputs':
-    [{'formula': '343.3 != ""',
-    'triggered': 'true',
-    'validation': 'Value present',
-    'reference': '70001',
-    'period': '201912',
-    'survey': '003',
-    'validationid': 23412,
-    'bpmid': ''},
-    {'formula': '12.3 != ""',
-    'triggered': 'true',
-    'validation': 'Value present',
-    'reference': '70001',
-    'period': '201912',
-    'survey': '003',
-    'validationid': 23413,
-    'bpmid': ''}]}
+def json_output():
+    return {'validation_outputs':
+        [{'formula': '343.3 != ""',
+        'triggered': True,
+        'validation': 'Value present',
+        'reference': '70001',
+        'period': '201912',
+        'survey': '003',
+        'validationid': 23412,
+        'bpmid': ''},
+        {'formula': '12.3 != ""',
+        'triggered': True,
+        'validation': 'Value present',
+        'reference': '70001',
+        'period': '201912',
+        'survey': '003',
+        'validationid': 23413,
+        'bpmid': ''}]}
 
-def test_output_data(input_data, output_data):
-    returned_data = parse_validation_save_data(input_data)
-    assert  returned_data == output_data
+
+def test_output_data(json_input, json_output):
+    returned_data = parse_validation_save_data(json_input)
+    print('return', returned_data)
+    assert returned_data == json_output
