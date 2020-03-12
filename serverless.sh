d52c7c26fb031857fd800f1f5dc96aca3aafe0a5
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-03-12 12:58:13 +0000

    Revert "Modified to just deploy code"
    This reverts commit 5eb06998901938cf552d2830b35919b41a1aeb7d.

--- serverless.sh
+++ serverless.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+cd save-deploy-repository
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
-serverless deploy --function main;
+serverless deploy --verbose;
