5eb06998901938cf552d2830b35919b41a1aeb7d
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-03-12 08:32:54 +0000

    Modified to just deploy code

--- serverless.sh
+++ serverless.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-cd save-deploy-repository
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
-serverless deploy --verbose;
+serverless deploy --function main;
